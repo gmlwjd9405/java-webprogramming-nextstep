17ca62f25d07f7a39836879d27ff4185915f55a7
kwonheejeong <gmlwjd9405@naver.com>
2017-11-21 19:27:38 +0900

    Modify test code with Junit

--- chapter2-calculator/src/CalculatorTest.java
+++ chapter2-calculator/src/CalculatorTest.java
@@ -1,26 +1,28 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
 public class CalculatorTest {
-    public static void main(String[] args) {
+    @Test
+    public void add(){
         Calculator cal = new Calculator();
-        add(cal);
-        subtract(cal);
-        multiply(cal);
-        divide(cal);
+        System.out.println(cal.add(6, 3));
     }
 
-    private static void divide(Calculator cal) {
-        System.out.println(cal.divide(9, 3));
+    @Test
+    public void subtract(){
+        Calculator cal = new Calculator();
+        System.out.println(cal.subtract(6, 3));
     }
 
-    private static void multiply(Calculator cal) {
-        System.out.println(cal.multiply(9, 3));
+    @Test
+    public void multiply(){
+        Calculator cal = new Calculator();
+        System.out.println(cal.multiply(6, 3));
     }
 
-    private static void subtract(Calculator cal) {
-        System.out.println(cal.subtract(9, 3));
-    }
-
-    private static void add(Calculator cal) {
-        System.out.println(cal.add(9, 3));
+    @Test
+    public void divide(){
+        Calculator cal = new Calculator();
+        System.out.println(cal.divide(6, 3));
     }
 }
-
