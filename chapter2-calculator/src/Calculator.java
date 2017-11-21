ead51827a8fea88cc7b10f25fbac412ac3a4b0fc
kwonheejeong <gmlwjd9405@naver.com>
2017-11-21 18:55:41 +0900

    Separate test code

--- chapter2-calculator/src/Calculator.java
+++ chapter2-calculator/src/Calculator.java
@@ -1,5 +1,3 @@
-import com.sun.org.apache.xpath.internal.SourceTreeManager;
-
 public class Calculator {
     int add(int i, int j){
         return i + j;
@@ -9,7 +7,7 @@ public class Calculator {
         return i - j;
     }
 
-    int mutiply(int i, int j){
+    int multiply(int i, int j){
         return i * j;
     }
 
@@ -17,11 +15,4 @@ public class Calculator {
         return i / j;
     }
 
-    public static void main(String[] args){
-        Calculator cal = new Calculator();
-        System.out.println(cal.add(3, 4));
-        System.out.println(cal.subtract(5,4));
-        System.out.println(cal.mutiply(2, 6));
-        System.out.println(cal.divide(8, 4));
-    }
 }
